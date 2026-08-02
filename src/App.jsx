f45197ad1c0827d73a25c9cc0b9202dc30bc98d9
itsRev1L <iamirhyd13@gmail.com>
2026-08-02 18:19:30 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div className="text-indigo-600">
+        <h1>Hello</h1>
+      </div>
+    </>
+  );
+};
+
+export default App;
