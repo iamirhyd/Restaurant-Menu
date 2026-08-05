584453cf21b934739ea866571bef84bb57fd7c35
itsRev1L <iamirhyd13@gmail.com>
2026-08-05 18:22:46 +0330

    landing page component

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { MdOutlineRestaurantMenu } from "react-icons/md";
+
+const LandingPage = () => {
+  return (
+    <div className="flex flex-col md:flex-row min-h-screen items-center ">
+      <div className="text-center w-full md:w-1/2 text-white px-4 md:px-12 space-y-4 pt-20">
+        <h1 className="font-extrabold text-4xl md:text-7xl text-left py-5 gap-4">
+          THE MOST <br />
+          <span className="text-amber-600"> UNHEALTHIEST</span> FOOD IN
+          <br />
+          THE WORLD
+        </h1>
+        <p className="text-left text-gray-300 font-thin text-2xl gap-4 ">
+          Unhealthy? Absolutely. Delicious? Definitely.
+        </p>
+        <div className="mt-4 flex items-center md:items-start ">
+          <button
+            className=" 
+              flex items-center gap-2 px-6 py-3 bg-amber-600 hover:bg-amber-700 rounded-md
+              transition-colors cursor-pointer shadow-md "
+          >
+            <MdOutlineRestaurantMenu /> Check out the Menu!
+          </button>
+        </div>
+      </div>
+      <div className="flex justify-center items-center  w-full mt-20 md:w-1/2">
+        <img
+          src="/Burger.png"
+          alt="Background"
+          className="w-full max-w-md md:max-w-lg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default LandingPage;
--- src/Page/HomePage.jsx
+++ src/Page/HomePage.jsx
@@ -1,45 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import Navbar from "../Components/Navbar";
-import { MdOutlineRestaurantMenu } from "react-icons/md";
-
+import LandingPage from "../Components/LandingPage";
 const HomePage = () => {
   return (
     <>
+      {/* Bacground */}
       <div className="relative min-h-screen bg-linear-to-b from-[#0A0A0A] to-[#2C1810] overflow-hidden">
         <div className="absolute -top-25 -right-20 w-96 h-96 bg-amber-600/10 rounded-full blur-3xl pointer-events-none"></div>
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-56 h-56 bg-amber-400/5 rounded-full blur-3xl pointer-events-none"></div>
+        {/*Navbar*/}
         <div className="absolute z-20">
           <Navbar />
         </div>
-        <div className="flex flex-col md:flex-row min-h-screen items-center ">
-          <div className="text-center w-full md:w-1/2 text-white px-4 md:px-12 space-y-4 pt-20">
-            <h1 className="font-extrabold text-4xl md:text-7xl text-left py-5 gap-4">
-              THE MOST <br />
-              <span className="text-amber-600"> UNHEALTHIEST</span> FOOD IN
-              <br />
-              THE WORLD
-            </h1>
-            <p className="text-left text-gray-300 font-thin text-2xl gap-4 ">
-              Unhealthy? Absolutely. Delicious? Definitely.
-            </p>
-            <div className="mt-4 flex items-center md:items-start ">
-              <button
-                className=" 
-              flex items-center gap-2 px-6 py-3 bg-amber-600 hover:bg-amber-700 rounded-md
-              transition-colors cursor-pointer shadow-md "
-              >
-                <MdOutlineRestaurantMenu /> Check out the Menu!
-              </button>
-            </div>
-          </div>
-          <div className="flex justify-center items-center  w-full mt-20 md:w-1/2">
-            <img
-              src="/Burger.png"
-              alt="Background"
-              className="w-full max-w-md md:max-w-lg"
-            />
-          </div>
-        </div>
+        <LandingPage />
       </div>
     </>
   );
