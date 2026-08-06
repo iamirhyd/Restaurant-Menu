4339cf185988b1b6cf292e9f1553ea7520092538
itsRev1L <iamirhyd13@gmail.com>
2026-08-06 19:58:08 +0330

    redirecting some code into components

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,27 +1,11 @@
 import React, { useState } from "react";
+import MenuList from "./MenuList";
 
 const Menu = () => {
-  const [selectItem, setSelectItem] = useState(null);
-  const Menu = ["BreakFast", "Launch", "Dinner", "Sides", "Dessert"];
   return (
-    <div className="bg-white min-h-screen w-full overflow-x-auto whitespace-nowrap">
+    <div className="bg-white min-h-screen w-full ">
       <div>
-        <ul className="gap-6 flex flex-row justify-evenly py-5 font-medium ">
-          {Menu.map((MenuList, i) => (
-            <li
-              key={MenuList}
-              onClick={() => {
-                setSelectItem(i);
-              }}
-              className={`hover:text-amber-600 group relative cursor-pointer ${selectItem === i ? "text-amber-600" : null}`}
-            >
-              {MenuList}
-              <span
-                className={`absolute -bottom-1 left-0 w-0 h-0.5 rounded-md bg-amber-600 transition-all duration-300 group-hover:w-full ${selectItem === i ? "w-full" : null}`}
-              ></span>
-            </li>
-          ))}
-        </ul>
+        <MenuList />
       </div>
     </div>
   );
---
+++ src/Components/MenuList.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+const MenuList = () => {
+  const [selectItem, setSelectItem] = useState(null);
+  const Menu = ["BreakFast", "Launch", "Dinner", "Sides", "Dessert"];
+  return (
+    <div className="overflow-x-auto whitespace-nowrap">
+      <ul className="gap-6 flex flex-row justify-evenly py-5 font-medium ">
+        {Menu.map((MenuList, i) => (
+          <li
+            key={MenuList}
+            onClick={() => {
+              setSelectItem(i);
+            }}
+            className={`hover:text-amber-600 group relative cursor-pointer ${selectItem === i ? "text-amber-600" : null}`}
+          >
+            {MenuList}
+            <span
+              className={`absolute -bottom-1 left-0 w-0 h-0.5 rounded-md bg-amber-600 transition-all duration-300 group-hover:w-full ${selectItem === i ? "w-full" : null}`}
+            ></span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MenuList;
